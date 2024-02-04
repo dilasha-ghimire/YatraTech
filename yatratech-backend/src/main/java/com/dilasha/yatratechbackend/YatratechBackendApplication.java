eedbaa0d97337c55d603dbdcf8f450c28b26b86a
dilasha-ghimire <dilashhaa@gmail.com>
2024-02-04 21:41:24 +0545

    backend setup

---
+++ yatratech-backend/src/main/java/com/dilasha/yatratechbackend/YatratechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dilasha.yatratechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatratechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatratechBackendApplication.class, args);
+	}
+
+}
